998450487707d7a6ec6cea923d61ae449b9eb330
stefanos-paschalis <stefan.paschalis@gmail.com>
2020-04-30 12:05:55 +0300

    new exercise

--- ClickoMania.cpp
+++ ClickoMania.cpp
@@ -29,6 +29,28 @@ BRRRBRRRBB
 BBBBRBRRRB
 BRBBBBBRBB
 RRRRRBBRRR
+
+ 20 10 3
+BBRBYBBRRB
+YBYBYBRRBB
+YYRRYYYBBB
+YRRRYRBBBB
+RRRYYBYYYR
+YBBYYRYYBB
+RRYRYYBRBY
+RRRRYRYYRY
+BRRYYRRYYB
+RRYRBBBRRY
+RBYBYRYYRR
+BYBYYBRYBY
+BRRBRYRYRB
+RBYRRYBYYY
+BRYYRYRRBY
+BRRYYBYRBY
+YBYYBYBBYB
+RBYYRRYBRB
+BYYBYYRBYB
+YBRBYYRRRR
 */
 
 struct Point {
@@ -89,15 +111,27 @@ struct Game {
         return result;
     }
     Point next_move(int rows, int cols, int num_colors, vector<string> &grid) {
+        Point point;
+        int max_cells = 0;
         vector<char> colors = get_distinct_colors(grid);
         multimap<int, Point, greater<int>> maps;
         for(int r=0; r<grid.size(); r++) {
             for(int c=0; c<grid[r].length(); c++) {
                 int cells = get_adjacent_cells(grid, r, c, grid[r][c], true);
+                if (cells >= max_cells) {
+                    max_cells = cells;
+                }
                 Point point(r, c);
                 maps.insert(make_pair(cells, point));
             }
         }
+        /*int max_cells_counter = 0;
+        for(auto m : maps) {
+            if (m.first == max_cells) {
+                point = m.second;
+            }
+        }
+        return point;*/
         return maps.begin()->second;
     }
     void delete_adjacent_cells(vector<string> &grid, int row, int col, char color) {
@@ -161,7 +195,7 @@ struct Game {
                 for(int r=0; r<grid.size(); r++) {
                     grid[r].erase(c, 1);
                 }
-                c--;
+                //c--;
             }
         }
     }
@@ -176,8 +210,26 @@ struct Game {
         }
         return result;
     }
+    int get_blocks(vector<string> &grid) {
+        int result = 0;
+        vector<string> grid_copy;
+        for(int r=0; r<grid.size(); r++) {
+            grid_copy.push_back(grid[r]);
+        }
+        for(int r=0; r<grid_copy.size(); r++) {
+            for(int c=0; c<grid_copy[r].length(); c++) {
+                if (grid_copy[r][c] != '-') {
+                    result++;
+                    delete_adjacent_cells(grid_copy, r, c, grid_copy[r][c]);
+                }
+            }
+        }
+        return result;
+    }
     void play_game(int rows, int cols, int num_colors, vector<string> &grid) {
         int round = 0;
+        int score = 0;
+        int blocks = get_blocks(grid);
         while(get_cells(grid) >= 1) {
             cout << "####### ROUND " << ++round << " #######" << endl;
             print_grid(grid);
@@ -186,6 +238,8 @@ struct Game {
             delete_adjacent_cells(grid, cell.row, cell.col, grid[cell.row][cell.col]);
             down_falling_cells(grid);
             left_falling_cells(grid);
+            score = (1 - get_blocks(grid)/blocks) * 5 * get_distinct_colors(grid).size();
+            cout << "score: " << score << endl;
         }
     }
 };
@@ -193,27 +247,8 @@ struct Game {
 void nextMove(int x, int y, int color, vector <string> grid){
     //your logic here
     Game game;
-    vector<char> colors = game.get_distinct_colors(grid);
-    multimap<int, Point, greater<int>> maps;
-    for(int r=0; r<grid.size(); r++) {
-         for(int c=0; c<grid[r].length(); c++) {
-            int cells = game.get_adjacent_cells(grid, r, c, grid[r][c], true);
-            Point point(r, c);
-            maps.insert(make_pair(cells, point));
-            //cout << r << ", " << c << ", color: " << grid[r][c] << ", cells: " << cells << endl;
-        }
-    }
-    cout << "max cells: " << maps.begin()->first << ", point: "
-        << maps.begin()->second.row << ", " << maps.begin()->second.col << endl;
-    //cout << maps.begin()->second.row << " " << maps.begin()->second.col << endl;
-
-
-    int row = maps.begin()->second.row;
-    int col = maps.begin()->second.col;
-    game.delete_adjacent_cells(grid, row, col, grid[row][col]);
-    for(int r=0; r<grid.size(); r++) {
-        cout << grid[r] << endl;
-    }
+    Point cell = game.next_move(x, y, color, grid);
+    game.print_cell(cell);
 }
 
 int main(void) {
